9d4b1884018d8202276972707b72b120258cbf2d
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-07-25 14:45:56 +0600

    module 6 start

---
+++ mod_6/math.js
@@ -0,0 +1,19 @@
+/**
+ * Math needed for porogramming
+ * 
+ * 1.  Number: [+ve,-ve,0]: 78,-50
+ * 2.  Fraction: 0.5, 25.13
+ * 3.  Even, Odd: 15,432
+ * 4.  Reminder: after dividing a  number
+ * 5.  Basic Math operation: add, deduct,multiply, divide
+ * 6.  Percentage: %
+ * 7.  Sum: 
+ * 8.  Avg: 10,20,30
+ * 9.  Max/Min: 45,21.87
+ * 10. Series: 0-10, 11-20
+ * 11. Calculation: Area od Triangle- 1/2 *base *height
+ * 12. Share profit: 3
+ * 13. Permutation:
+ * 14. Fibonnaci Series:
+ * 15. Modulas: % to find out reminder
+ */
---
+++ mod_6/variable.js
@@ -0,0 +1,23 @@
+// Variable
+// var -> All kinds of data
+// const --> Fixed data can be saved. the stored data or value here can't be changed 
+// let 
+
+var weight=38;
+var temperature= 38;
+var people=38;
+
+console.log(temperature);
+
+let price= 100;
+price=50;
+price=299;
+
+const name="Kala mia";  //fixed
+const PI=3.14; // fixed
+
+
+// var can be accessed from anywhere 
+
+// let can be used within the declared area 
+
