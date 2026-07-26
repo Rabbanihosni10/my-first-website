7b84322bfc2650f81179e7b92e14633c676946e2
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-07-26 07:48:34 +0600

    naming convention

---
+++ mod_6/naming.js
@@ -0,0 +1,28 @@
+let location= "Banani";
+
+let age= 35;
+let boyosh= 18;
+let togotogtogotgtogtogto= 97; 
+
+
+//variable name should be something meaningful.
+// it will indicate what it contains
+
+let avc=45;
+let info="mon majar";
+
+//do not use shorthand 
+//its okey if the name us longer
+
+
+//snake case
+const her_phone_nimer=122323423;
+// Camel case
+const herPhoneNumber=2123132321231;
+//Pascal Case
+const HerPhoneNumber=12123213123;
+
+
+//name can't start with a number
+
+
