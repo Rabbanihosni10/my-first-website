72edb9732fe4079dd7007c983203fc87f764a949
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-07-26 22:06:29 +0600

    conditional, and and or

---
+++ mod_7/else.js
@@ -0,0 +1,43 @@
+/**
+ * if it is rainging
+ *      I will take the umbrella 
+ * if not 
+ *      I will take extra books in my hand 
+*/
+
+
+/**
+ * 
+ * 
+ * if(condition){
+ * }
+ * 
+ * else if(condition){
+ * }
+ * 
+ * else{
+ * 
+ * }
+ * 
+*/
+
+
+const rickshawFair=20;
+
+if(rickshawFair<50){
+    console.log("Mama hood tule den");
+    console.log("Mama aste jan");
+}
+else{
+    console.log("Na mama jan taile apni");
+}
+
+const x=200;
+const y=200;
+if(x!==y){
+    const z=x+y;
+    console.log(z);
+}
+else{
+    console.log("They are equal mukul ukul");
+}
