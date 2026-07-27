8e9a46443781684c50c7fe40b6cea12b4de2c4e6
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-07-27 09:06:03 +0600

    practice2.0

---
+++ mod_6/practice.js
@@ -0,0 +1,47 @@
+// 
+
+
+let fullName = "Hosni Rabbani";
+let age = 24;
+let country = "Bangladesh";
+let favoriteLanguage = "JavaScript";
+let dreamJob = "Full-Stack Developer";
+let yearsToGoal = 1;
+let isLearningJS = true;
+ 
+console.log("========================");
+console.log("     MY PROFILE");
+console.log("========================");
+console.log();
+console.log("Name:", fullName);
+console.log("Age:", age);
+console.log("Country:", country);
+console.log("Favorite Language:", favoriteLanguage);
+console.log("Dream Job:", dreamJob);
+console.log("Years to Reach Goal:", yearsToGoal);
+console.log("Learning JavaScript:", isLearningJS);
+console.log();
+console.log("========================");
+
+
+
+let productName="Hub";
+let price=3000;
+let quantity=15;
+let taxParcent=15;
+
+let subTotal= quantity*price;
+let taxAmount=(subTotal *taxParcent)/100;
+let grandTotal=subTotal+taxAmount;
+
+
+
+
+console.log("---- Shopping Bill ----");
+console.log("Product:", productName);
+console.log("Price per unit:", price);
+console.log("Quantity:", quantity);
+console.log("Subtotal:", subTotal);
+console.log("Tax (" + taxParcent + "%):", taxAmount);
+console.log("Grand Total:", grandTotal);
+console.log("------------------------");
