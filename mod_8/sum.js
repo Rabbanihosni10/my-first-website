e4acf2c216a182bebc2fa37b00a7b53b267b2b9a
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-07-30 04:00:34 +0600

    decremental and table

---
+++ mod_8/sum.js
@@ -0,0 +1,8 @@
+//1,2,3,4,5
+//sum=?
+
+let count=0;
+for(let i=0;i<=10;i++){
+    count+=i;
+    console.log(i,count);
+}
---
+++ mod_8/table.js
@@ -0,0 +1,6 @@
+const number=3;
+
+console.log("Table of",number,"-->");
+for(let i=1; i<=10;i++){
+    console.log(`${number}X${i} = ${number*i}`);
+}
