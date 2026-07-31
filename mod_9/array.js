5aa877a90876d9549cb7acd3d09fb8d539a44d86
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-07-31 22:11:48 +0600

    arrary srt

---
+++ mod_9/array.js
@@ -0,0 +1,2 @@
+const arr=[1,23,4345,65,0];
+console.log(arr);
---
+++ mod_9/recap.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * 1.Variable
+ * 2.
+ */
+
+const price=50;
+const studentName='Jabbar Kha';
+const isRich=true;
+// 2. Conditionals
+
+if(studentName=="Jabbar Khan"){
+    
+}
+
+for(let i=0; i<5;r++){
+    
+}
