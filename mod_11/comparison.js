a1b7cf0f98509da954ffc98d73fa2e6b8b664b33
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-08-05 15:08:30 +0600

    trim,trimStart,trimEnd,toUpperCase,toLowerCase

--- mod_11/comparison.js
+++ mod_11/comparison.js
@@ -5,7 +5,8 @@ console.log(book);
 console.log(book.toLowerCase());
 console.log("================");
 console.log(subject);
-if(subject===book){
+console.log(subject.toUpperCase());
+if(subject===book.toLowerCase()){
     console.log("Poire pora fatai felbo");
 }
 else{
@@ -15,3 +16,23 @@ else{
 //Js is case sensitive
 
 
+const emailUserProvided='goni@Mia.com ';
+const emailSavedInDatavase='  goni@mia.com  ';
+console.log("#######"+emailSavedInDatavase+"######");
+console.log("#######"+emailSavedInDatavase.trimStart()+"######");
+console.log("#######"+emailSavedInDatavase.trimEnd()+"######");
+console.log("#######"+emailSavedInDatavase.trimEnd().trimStart()+"######");
+console.log("#######"+emailSavedInDatavase.trim().toUpperCase()+"######");
+
+if(emailUserProvided === emailSavedInDatavase){
+    console.log("Welcome to the website!");
+}
+else{
+    console.log("DGM---> Duur e jeye morobba kha!");
+}
+
+// const emailUserProvided='goni@Mia.com';
+// const emailSavedInDatavase='goni@mia.com';
+// if(emailUserProvided.toLowerCase()===emailSavedInDatavase.toLowerCase()){
+       
+// }
