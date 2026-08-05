3212d504547f8ba25f8b773138ae6873b0523f3d
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-08-05 18:30:51 +0600

    object start

---
+++ mod_11/object.js
@@ -0,0 +1,28 @@
+// const cap=['Khaki','CR7', 250, 200];
+/**
+ * name: chosma
+ * price: 300
+ * color: white
+ * 
+ */
+
+const sunglass= {
+    color: 'White',
+    price: 120,
+    brand: 'Ray Ban',
+};
+
+
+const phone={
+    brand: 'Nokia',
+    price: 25,
+    color: 'Black',
+    isThreeCamera: false,
+    appInstalled: ['FB','Chatgpt','Neptune'],
+    isSecondHand: false
+}
+console.log(sunglass);
+console.log(phone);
+
+
+// object sometimes have some functions inside it . it is named as object mathode
