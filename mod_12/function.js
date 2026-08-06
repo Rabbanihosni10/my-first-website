dd59ab15df91f9f7eda22f9c66f0ed601d96cfc2
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-08-07 05:31:20 +0600

    revise before enter function and basic function

---
+++ mod_12/function.js
@@ -0,0 +1,34 @@
+// function
+/**
+ * give a name
+ * give two bracket(1st)
+ *  aftern 1st bracket , give two second bracket or peranthesis
+ * inside the second bracket you will do the work for this function
+ * 
+ * function declaration
+ */
+
+function fanOnKor(){
+    console.log("Step-1: Walk towards the switch");
+    console.log("Step-2: Press the switch");
+    console.log("Step-3: Come back and do whatever you were doing");
+}
+function setTheMosquitoNet(){
+    console.log("Mosquito no dinner for you!!!");
+}
+
+
+// call the function
+//the the function name and then two first bracket
+fanOnKor();
+setTheMosquitoNet();
+
+function add(a,b){
+    const add=a+b;
+    return add;
+}
+const sum=add(2,3);
+
+
+console.log(sum);
+
