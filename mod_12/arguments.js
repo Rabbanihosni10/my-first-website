685a237e5c10bbeb2d6904873df885734235fb94
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-08-07 07:20:01 +0600

    argument and return knowledge

---
+++ mod_12/arguments.js
@@ -0,0 +1,15 @@
+// arguments
+
+function multiply(a,b){ //parameter
+    const result= a*b;
+    console.log("Result of the multiplication is:",result);
+}
+
+multiply(3,5,23,13,35);  //arguments
+
+function square(number){
+    const result=number* number;
+    console.log("Square of",number,"is:",result);
+}
+
+square(7);
