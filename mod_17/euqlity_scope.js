f14b677807d747bbd6a53bb6a849f631edfc2f81
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-08-14 19:06:51 +0600

    euqality and scope and hosting

---
+++ mod_17/euqlity_scope.js
@@ -0,0 +1,31 @@
+// == loose equality  // value are compared or matched
+// === strict equality.  // type , value are compared or matched
+
+
+
+console.log(5=="5");
+console.log(5==="5");
+
+console.log(0 == false);
+console.log(0 === false);
+
+let tobarok=15;
+if(true){
+    let age=54;
+    console.log(tobarok);
+    tobarok=20;
+    console.log(tobarok);
+}
+
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(5,15));
+
+
+// var 
+// let 
+// const 
+// function 
